5acf161071796683d1c65ddb02a3ca6515bfd381
shibing624 <shibing624@126.com>
2022-05-10 19:57:23 +0800

    update model init.

--- textgen/seq2seq/__init__.py
+++ textgen/seq2seq/__init__.py
@@ -6,3 +6,5 @@
 
 from textgen.config.model_args import Seq2SeqArgs
 from textgen.seq2seq.bert_seq2seq_model import BertSeq2SeqModel
+from textgen.seq2seq.seq2seq_model import Seq2SeqModel
+from textgen.seq2seq.conv_seq2seq_model import ConvSeq2SeqModel
