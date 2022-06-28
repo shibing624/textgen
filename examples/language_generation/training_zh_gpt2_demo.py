a018a83be7eff17c84f1cd51913ab7349ab03110
shibing624 <shibing624@126.com>
2022-06-28 14:12:20 +0800

    update gpt2 model train demo.

--- examples/language_generation/training_zh_gpt2_demo.py
+++ examples/language_generation/training_zh_gpt2_demo.py
@@ -16,6 +16,7 @@ from textgen.language_modeling import LanguageModelingModel
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_file', default='../data/zh_article.txt', type=str, help='Training data file')
+    parser.add_argument('--test_file', default='../data/zh_article.txt', type=str, help='Test data file')
     parser.add_argument('--model_type', default='gpt2', type=str, help='Transformers model type')
     parser.add_argument('--model_name', default='ckiplab/gpt2-base-chinese', type=str,
                         help='Transformers model or path')
@@ -42,11 +43,11 @@ def main():
             "num_train_epochs": args.num_epochs,
             "mlm": False,
             "output_dir": args.output_dir,
-            "dataset_type": "text",
         }
 
         model = LanguageModelingModel(args.model_type, args.model_name, args=train_args)
-        model.train_model(args.train_file)
+        model.train_model(args.train_file, eval_file=args.test_file)
+        print(model.eval_model(args.test_file))
 
     if args.do_predict:
         logger.info('Predict...')
