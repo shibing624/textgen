eed5ef08769cba01bac503c0a2804f9625013de7
shibing624 <shibing624@126.com>
2022-06-28 12:07:59 +0800

    update gpt2 text train.

--- examples/language_generation/training_couplet_gpt2_demo.py
+++ examples/language_generation/training_couplet_gpt2_demo.py
@@ -3,6 +3,8 @@
 @author:XuMing(xuming624@qq.com)
 @description: 
 """
+from loguru import logger
+import argparse
 from transformers import BertTokenizerFast
 import sys
 
@@ -11,55 +13,61 @@ from textgen.language_generation import LanguageGenerationModel
 from textgen.language_modeling import LanguageModelingModel
 
 
-def raw(prompts):
-    model = LanguageGenerationModel("gpt2", "ckiplab/gpt2-base-chinese", args={"max_length": 64})
-    for prompt in prompts:
-        # Generate text using the model. Verbose set to False to prevent logging generated sequences.
-        generated = model.generate(prompt, verbose=False)
-        generated = generated[0]
-        print("=" * 42)
-        print(generated)
-        print("=" * 42)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_file', default='../data/couplet/train.txt', type=str, help='Training data file')
+    parser.add_argument('--test_file', default='../data/couplet/test.txt', type=str, help='Test data file')
+    parser.add_argument('--model_type', default='gpt2', type=str, help='Transformers model type')
+    parser.add_argument('--model_name', default='ckiplab/gpt2-base-chinese', type=str,
+                        help='Transformers model or path')
+    parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
+    parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
+    parser.add_argument('--output_dir', default='./outputs/couplet-fine-tuned/', type=str,
+                        help='Model output directory')
+    parser.add_argument('--max_seq_length', default=50, type=int, help='Max sequence length')
+    parser.add_argument('--num_epochs', default=10, type=int, help='Number of training epochs')
+    parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
+    args = parser.parse_args()
+    logger.info(args)
 
+    if args.do_train:
+        logger.info('Training...')
+        train_args = {
+            "reprocess_input_data": True,
+            "overwrite_output_dir": True,
+            "block_size": 512,
+            "max_seq_length": args.max_seq_length,
+            "learning_rate": 5e-6,
+            "train_batch_size": args.batch_size,
+            "gradient_accumulation_steps": 8,
+            "num_train_epochs": args.num_epochs,
+            "mlm": False,
+            "output_dir": args.output_dir,
+        }
 
-def finetune(prompts, train_path, test_path):
-    train_args = {
-        "reprocess_input_data": True,
-        "overwrite_output_dir": True,
-        "block_size": 512,
-        "max_seq_length": 64,
-        "learning_rate": 5e-6,
-        "train_batch_size": 8,
-        "gradient_accumulation_steps": 8,
-        "num_train_epochs": 10,
-        "mlm": False,
-        "output_dir": f"outputs/couplet-fine-tuned/",
-    }
+        model = LanguageModelingModel(args.model_type, args.model_name, args=train_args)
+        model.train_model(args.train_path, eval_file=args.test_path)
+        print(model.eval_model(args.test_path))
 
-    model = LanguageModelingModel("gpt2", "ckiplab/gpt2-base-chinese", args=train_args)
-    model.train_model(train_path, eval_file=test_path)
-    print(model.eval_model(test_path))
+    if args.do_predict:
+        logger.info('Predict...')
+        # Use fine-tuned model
+        tokenizer = BertTokenizerFast.from_pretrained(args.output_dir)
+        model = LanguageGenerationModel(args.model_type, args.output_dir, args={"max_length": args.max_seq_length},
+                                        tokenizer=tokenizer)
 
-    # Use fine-tuned model
-    tokenizer = BertTokenizerFast.from_pretrained("outputs/couplet-fine-tuned")
-    model = LanguageGenerationModel("gpt2", "outputs/couplet-fine-tuned", args={"max_length": 64}, tokenizer=tokenizer)
-    for prompt in prompts:
-        # Generate text using the model. Verbose set to False to prevent logging generated sequences.
-        generated = model.generate(prompt, verbose=False)
-        generated = generated[0]
-        print("=" * 42)
-        print(generated)
-        print("=" * 42)
+        couplet_prompts = [
+            "晚风摇树树还挺，",
+            "深院落滕花，石不点头龙不语，",
+            "不畏鸿门传汉祚，"
+        ]
+        for prompt in couplet_prompts:
+            # Generate text using the model. Verbose set to False to prevent logging generated sequences.
+            generated = model.generate(prompt, verbose=False)
+            generated = generated[0]
+            print("inputs:", prompt)
+            print("outputs:", generated)
 
 
 if __name__ == '__main__':
-    prompts = [
-        "晚风摇树树还挺",
-        "深院落滕花，石不点头龙不语",
-        "不畏鸿门传汉祚"
-    ]
-    raw(prompts)
-
-    train_path = "../data/couplet/train.txt"
-    test_path = "../data/couplet/test.txt"
-    finetune(prompts, train_path, test_path)
+    main()
--- examples/language_generation/training_zh_gpt2_demo.py
+++ examples/language_generation/training_zh_gpt2_demo.py
@@ -3,6 +3,8 @@
 @author:XuMing(xuming624@qq.com)
 @description: 
 """
+from loguru import logger
+import argparse
 from transformers import BertTokenizerFast
 import sys
 
@@ -11,63 +13,70 @@ from textgen.language_generation import LanguageGenerationModel
 from textgen.language_modeling import LanguageModelingModel
 
 
-def raw(prompts):
-    model = LanguageGenerationModel("gpt2", "ckiplab/gpt2-base-chinese", args={"max_length": 64})
-    for prompt in prompts:
-        # Generate text using the model. Verbose set to False to prevent logging generated sequences.
-        generated = model.generate(prompt, verbose=False)
-        generated = generated[0]
-        print("=" * 42)
-        print(generated)
-        print("=" * 42)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_file', default='../data/zh_article.txt', type=str, help='Training data file')
+    parser.add_argument('--model_type', default='gpt2', type=str, help='Transformers model type')
+    parser.add_argument('--model_name', default='ckiplab/gpt2-base-chinese', type=str,
+                        help='Transformers model or path')
+    parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
+    parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
+    parser.add_argument('--output_dir', default='./outputs/couplet-fine-tuned/', type=str,
+                        help='Model output directory')
+    parser.add_argument('--max_seq_length', default=50, type=int, help='Max sequence length')
+    parser.add_argument('--num_epochs', default=10, type=int, help='Number of training epochs')
+    parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
+    args = parser.parse_args()
+    logger.info(args)
 
+    if args.do_train:
+        logger.info('Training...')
+        train_args = {
+            "reprocess_input_data": True,
+            "overwrite_output_dir": True,
+            "block_size": 512,
+            "max_seq_length": args.max_seq_length,
+            "learning_rate": 5e-6,
+            "train_batch_size": args.batch_size,
+            "gradient_accumulation_steps": 8,
+            "num_train_epochs": args.num_epochs,
+            "mlm": False,
+            "output_dir": args.output_dir,
+            "dataset_type": "text",
+        }
 
-def finetune(prompts, train_path, test_path):
-    train_args = {
-        "reprocess_input_data": True,
-        "overwrite_output_dir": True,
-        "block_size": 512,
-        "max_seq_length": 64,
-        "learning_rate": 5e-6,
-        "train_batch_size": 8,
-        "gradient_accumulation_steps": 8,
-        "num_train_epochs": 3,
-        "mlm": False,
-        "output_dir": f"outputs/zh-fine-tuned/",
-    }
+        model = LanguageModelingModel(args.model_type, args.model_name, args=train_args)
+        model.train_model(args.train_path, eval_file=args.test_path)
+        print(model.eval_model(args.test_path))
 
-    model = LanguageModelingModel("gpt2", "ckiplab/gpt2-base-chinese", args=train_args)
-    model.train_model(train_path, eval_file=test_path)
-    print(model.eval_model(test_path))
+    if args.do_predict:
+        logger.info('Predict...')
+        # Use fine-tuned model
+        tokenizer = BertTokenizerFast.from_pretrained(args.output_dir)
+        model = LanguageGenerationModel(args.model_type, args.output_dir, args={"max_length": args.max_seq_length},
+                                        tokenizer=tokenizer)
 
-    # Use fine-tuned model
-    tokenizer = BertTokenizerFast.from_pretrained("outputs/zh-fine-tuned")
-    model = LanguageGenerationModel("gpt2", "outputs/zh-fine-tuned", args={"max_length": 64}, tokenizer=tokenizer)
-    for prompt in prompts:
-        # Generate text using the model. Verbose set to False to prevent logging generated sequences.
-        generated = model.generate(prompt, verbose=False)
-        generated = generated[0]
-        print("=" * 42)
-        print(generated)
-        print("=" * 42)
+        """
+        王语嫣知道表哥神智已乱，富贵梦越做越深，不禁凄然。
+
+        段誉见到阿碧的神情，怜惜之念大起，只盼招呼她和慕容复回去大理，妥为安顿，却见她瞧着慕容复的眼色中柔情无限，
+        而慕容复也是一副志得意满之态，心中登时一凛：“各有各的缘法，慕容兄与阿碧如此，我觉得他们可怜，其实他们心中，
+        焉知不是心满意足？我又何必多事？”轻轻拉了拉王语嫣的衣袖，做个手势。
+
+        众人都悄悄退了开去。但见慕容复在土坟上南面而坐，口中兀自喃喃不休。
+        """
+        prompts = [
+            "王语嫣知道表哥神智已乱",
+            "段誉见到阿碧的神情，怜惜之念大起，只盼",
+            "众人都悄悄退了开去。但见慕容复在土坟上南面而坐"
+        ]
+        for prompt in prompts:
+            # Generate text using the model. Verbose set to False to prevent logging generated sequences.
+            generated = model.generate(prompt, verbose=False)
+            generated = generated[0]
+            print("inputs:", prompt)
+            print("outputs:", generated)
 
 
 if __name__ == '__main__':
-    """
-    王语嫣知道表哥神智已乱，富贵梦越做越深，不禁凄然。
-
-    段誉见到阿碧的神情，怜惜之念大起，只盼招呼她和慕容复回去大理，妥为安顿，却见她瞧着慕容复的眼色中柔情无限，
-    而慕容复也是一副志得意满之态，心中登时一凛：“各有各的缘法，慕容兄与阿碧如此，我觉得他们可怜，其实他们心中，
-    焉知不是心满意足？我又何必多事？”轻轻拉了拉王语嫣的衣袖，做个手势。
-
-    众人都悄悄退了开去。但见慕容复在土坟上南面而坐，口中兀自喃喃不休。
-    """
-    prompts = [
-        "王语嫣知道表哥神智已乱",
-        "段誉见到阿碧的神情，怜惜之念大起，只盼",
-        "众人都悄悄退了开去。但见慕容复在土坟上南面而坐"
-    ]
-    raw(prompts)
-
-    train_path = "../data/zh_article.txt"
-    finetune(prompts, train_path, train_path)
+    main()
