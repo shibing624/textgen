98eb31d1d7072f9533b1409abad94bd9a7f28e04
shibing624 <shibing624@126.com>
2022-07-04 16:03:44 +0800

    update import file to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
--- textgen/__init__.py
+++ textgen/__init__.py
@@ -4,7 +4,7 @@
 @description: 
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from textgen.augment.text_augment import TextAugment
 
