0dff029125684ec462a48f2f621ed3503c73508a
shibing624 <shibing624@126.com>
2022-07-17 08:40:19 +0800

    update t5 pegasus.

--- examples/T5/t5pegasus.py
+++ examples/T5/t5pegasus.py
@@ -4,7 +4,6 @@
 @description: 中文句子续写
 refer https://github.com/renmada/t5-pegasus-pytorch
 原文:蓝蓝的天上有一朵白白的云
-bert4keras预测	《蓝蓝的天上有一朵白白的云》是蓝蓝的天上有一朵白白的云创作的网络小说，发表于...
 torch预测     	《蓝蓝的天上有一朵白白的云》是蓝蓝的天上有一朵白白的云创作的网络小说，发表于...
 """
 
@@ -18,13 +17,16 @@ from textgen.t5.t5_utils import ZHTokenizer
 # 模型名	MODEL_NAME
 # t5-pegasus-base	imxly/t5-pegasus
 # t5-pegasus-small	imxly/t5-pegasus-small
+# t5-copy	imxly/t5-copy
+# t5-copy-summary	imxly/t5-copy-summary
 
 # torch版本
-tokenizer = ZHTokenizer.from_pretrained("imxly/t5-pegasus")
-model = MT5ForConditionalGeneration.from_pretrained("imxly/t5-pegasus")
+model_name = 'imxly/t5-copy'
+tokenizer = ZHTokenizer.from_pretrained(model_name)
+model = MT5ForConditionalGeneration.from_pretrained(model_name)
 
 sents = [
-    "蓝蓝的天上有一朵白白的云，",
+    "蓝蓝的天上有一朵白白的云",
     "五一假期欢乐多，我这几天也玩开心了，放假第二天，我就和姐姐一起去了外公家，现在这季节正是插秧的好时机，",
     "我的心爱之物 我有一只可爱的珍珠鳖，我们有一段特别的感情。 记得它是几个月前，叔叔从河边给我带来的。这只小鳖是灰黑色的，"
 ]
@@ -38,5 +40,5 @@ for text in sents:
                             eos_token_id=tokenizer.sep_token_id,
                             top_k=1,
                             max_length=250).numpy()[0]
-    torch_res = ''.join(tokenizer.decode(output[1:])).replace(' ', '')
-    print(torch_res)
+    r = ''.join(tokenizer.decode(output[1:])).replace(' ', '')
+    print(r)
