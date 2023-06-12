58a41f0694df933af3674220ba9abba2bbbfd086
shibing624 <shibing624@126.com>
2023-06-12 11:14:24 +0800

    update bloom csc demo.

---
+++ examples/bloom/training_bloom_mydata_demo.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+import argparse
+import sys
+
+import pandas as pd
+from loguru import logger
+
+sys.path.append('../..')
+from textgen import BloomModel
+
+
+def load_data(file_path):
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip('\n')
+            terms = line.split('\t')
+            instruction = '对下面中文拼写纠错：'
+            if len(terms) == 2:
+                data.append([instruction, terms[0], terms[1]])
+            else:
+                logger.warning(f'line error: {line}')
+    return data
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_file', default="../data/zh_csc_train.tsv", type=str, help='Train file')
+    parser.add_argument('--model_type', default='bloom', type=str, help='Transformers model type')
+    parser.add_argument('--model_name', default='bigscience/bloomz-560m', type=str, help='Transformers model or path')
+    parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
+    parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
+    parser.add_argument('--output_dir', default='./outputs-csc-v1/', type=str, help='Model output directory')
+    parser.add_argument('--max_seq_length', default=128, type=int, help='Input max sequence length')
+    parser.add_argument('--max_length', default=128, type=int, help='Output max sequence length')
+    parser.add_argument('--num_epochs', default=1.0, type=float, help='Number of training epochs')
+    parser.add_argument('--batch_size', default=1, type=int, help='Batch size')
+    args = parser.parse_args()
+    logger.info(args)
+    model = None
+    # fine-tune chatGLM model
+    if args.do_train:
+        logger.info('Loading data...')
+        model_args = {
+            'use_peft': True,
+            "overwrite_output_dir": True,
+            "max_seq_length": args.max_seq_length,
+            "max_length": args.max_length,
+            "per_device_train_batch_size": args.batch_size,
+            "num_train_epochs": args.num_epochs,
+            "output_dir": args.output_dir,
+        }
+        model = BloomModel(args.model_type, args.model_name, args=model_args)
+        train_data = load_data(args.train_file)
+        logger.debug('train_data: {}'.format(train_data[:10]))
+        train_df = pd.DataFrame(train_data, columns=["instruction", "input", "output"])
+        eval_df = train_df[:10]
+        train_df = train_df[10:]
+        model.train_model(train_df, eval_data=eval_df)
+    if args.do_predict:
+        if model is None:
+            model = BloomModel(
+                args.model_type, args.model_name,
+                peft_name=args.output_dir,
+                args={'use_peft': True, 'eval_batch_size': args.batch_size, "max_length": args.max_length, }
+            )
+        sents = ['对下面中文拼写纠错：\n少先队员因该为老人让坐。\n答：',
+                 '对下面中文拼写纠错：\n下个星期，我跟我朋唷打算去法国玩儿。\n答：']
+        response = model.predict(sents)
+        print(response)
+
+
+if __name__ == '__main__':
+    main()
