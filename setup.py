5157ca190789c2e23b0e42f0cc60aa81915d315d
shibing624 <shibing624@126.com>
2023-08-02 11:39:01 +0800

    update to v1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='textgen',
-    version='1.0.1',
+    version='1.0.2',
     description='Text Generation Model',
     long_description=readme,
     long_description_content_type='text/markdown',
--- textgen/__init__.py
+++ textgen/__init__.py
@@ -4,7 +4,7 @@
 @description: 
 """
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from textgen.augment.text_augment import TextAugment
 
